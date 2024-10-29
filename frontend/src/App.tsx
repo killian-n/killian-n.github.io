d4a57abcd02f27724866a6e709f6c3ff0e647cad
Killian Nolan <killian.nolan1300@gmail.com>
2024-10-29 14:20:02 +1100

    Used the NavBar context to scroll to interests.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,13 @@ import HomePage from './pages/Home';
 import ProjectsPage from './pages/Projects';
 import ResumePage from './pages/Resume';
 import AboutPage from './pages/About';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { initParticlesEngine } from '@tsparticles/react';
-
-import { loadSlim } from '@tsparticles/slim'; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-
+import { loadSlim } from '@tsparticles/slim';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { NavContext } from './components/NavContext';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 function App() {
   const [init, setInit] = useState(false);
   useEffect(() => {
@@ -23,18 +23,29 @@ function App() {
     });
   }, []);
 
+  const navRef = useRef<HTMLElement | null>(null);
+
+  const [navHeight, setNavHeight] = useState(0);
+  useEffect(() => {
+    if (navRef.current) {
+      setNavHeight(navRef.current.clientHeight);
+    }
+  });
+
   if (init) {
     return (
       <>
         <ThemeProvider theme={Theme}>
           <Router>
-            <NavBar></NavBar>
-            <Routes>
-              <Route path="/" element={<HomePage></HomePage>} />
-              <Route path="/Projects" element={<ProjectsPage />} />
-              <Route path="/About" element={<AboutPage />} />
-              <Route path="/Resume" element={<ResumePage />} />
-            </Routes>
+            <NavContext.Provider value={navHeight}>
+              <NavBar ref={navRef}></NavBar>
+              <Routes>
+                <Route path="/" element={<HomePage></HomePage>} />
+                <Route path="/Projects" element={<ProjectsPage />} />
+                <Route path="/About" element={<AboutPage />} />
+                <Route path="/Resume" element={<ResumePage />} />
+              </Routes>
+            </NavContext.Provider>
           </Router>
         </ThemeProvider>
       </>
