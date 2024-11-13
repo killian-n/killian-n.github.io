7d8ef5f465408ca94fd6d46c012caa90b44a9e93
Killian Nolan <killian.nolan1300@gmail.com>
2024-11-14 01:11:26 +1100

    Updated styling to scale and centre content better

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -7,21 +7,35 @@ const ProjectCard = forwardRef<HTMLDivElement, ProjectCardProps>(
     return (
       <Card
         sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
           backgroundColor: 'red',
           marginInline: 'auto',
           maxWidth: 'fit-content',
           minWidth: '30%',
           margin: '10px',
+          textAlign: 'center',
         }}
         ref={ref}
       >
         <CardMedia
-          sx={{ maxWidth: '250px', marginTop: '10px', marginInline: 'auto' }}
+          sx={{
+            maxWidth: '100%', // Ensure it takes the full width of the card
+            height: 'auto', // Adjust height if needed to keep aspect ratio
+            marginTop: '10px',
+            marginInline: 'auto',
+          }}
           image={props.img}
           component="img"
-        ></CardMedia>
-        <CardContent>{props.children}</CardContent>
-        <CardActions></CardActions>
+        />
+        <CardContent
+          sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+        >
+          {props.children}
+        </CardContent>
+        <CardActions sx={{ justifyContent: 'center' }}></CardActions>
       </Card>
     );
   },
