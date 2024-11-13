7d2e5c085e0a6061b93abbe28166d35a6bbe8a63
Killian Nolan <killian.nolan1300@gmail.com>
2024-11-14 01:10:28 +1100

    Used navContext to position future content under.

--- frontend/src/pages/Resume.tsx
+++ frontend/src/pages/Resume.tsx
@@ -1,9 +1,13 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { NavContext } from '../components/NavContext';
+import { useContext } from 'react';
 
 export default function ResumePage() {
+  const navHeight = useContext(NavContext);
+  console.log(navHeight);
   return (
-    <>
+    <Box sx={{ marginTop: `${navHeight}px` }}>
       <Typography>Post Resume on this page</Typography>
-    </>
+    </Box>
   );
 }
