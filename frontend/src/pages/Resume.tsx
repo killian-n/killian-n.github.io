677096eb5c502ea0d163a1dc6f9624655e75d507
Killian Nolan <killian.nolan1300@gmail.com>
2025-01-09 15:29:04 +1100

    Added a rendered resume to the Resume page.

--- frontend/src/pages/Resume.tsx
+++ frontend/src/pages/Resume.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { NavContext } from '../components/NavContext';
 import { useContext } from 'react';
 
@@ -6,8 +6,17 @@ export default function ResumePage() {
   const navHeight = useContext(NavContext);
   console.log(navHeight);
   return (
-    <Box sx={{ marginTop: `${navHeight}px` }}>
-      <Typography>Post Resume on this page</Typography>
+    <Box
+      sx={{
+        marginTop: `${navHeight}px`,
+        width: '100%',
+        height: `calc(100vh - ${navHeight}px)`,
+      }}
+    >
+      <object data="cv.pdf" type="application/pdf" width="100%" height="100%">
+        Your browser does not support PDFs. Please download the file
+        <a href="cv.pdf">here</a>
+      </object>
     </Box>
   );
 }
